db4ab18ed6f7e102185506590a982bdd50323a77
sanjeev kumar gond <sskumar9128@gmail.com>
2024-10-05 23:35:16 +0530

    add sop message

--- src/main/java/com/example/demo/FileUploadOnFileCoinFullStackApplication.java
+++ src/main/java/com/example/demo/FileUploadOnFileCoinFullStackApplication.java
@@ -8,6 +8,7 @@ public class FileUploadOnFileCoinFullStackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileUploadOnFileCoinFullStackApplication.class, args);
+		System.out.println("Hello")l
 	}
 
 }
